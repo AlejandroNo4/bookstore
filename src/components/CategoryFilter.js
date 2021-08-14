6e6b40ff76a66b0579d08f2e2ebd32126b5ecdb2
Alejandro Contreras <kdta15@outlook.com>
2021-08-13 19:43:33 -0500

    Add CHANGE_FILTER and REMOVE_FILTER actions. Create filter reducer. Add select box to render the filtered books. Add logic to the filter.

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+export default function CategoryFilter({ selectHandler }) {
+  const categories = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const categoryList = categories.map((c) => <option key={c}>{c}</option>);
+  const handleChange = (val) => selectHandler(val);
+
+  return (
+    <form>
+      <p>Filter by category:</p>
+      <select name="categoryName" className="i-field" onChange={(e) => handleChange(e.target.value)}>
+        {categoryList}
+      </select>
+    </form>
+  );
+}
+
+CategoryFilter.defaultProps = {
+  selectHandler: null,
+};
+
+CategoryFilter.propTypes = {
+  selectHandler: PropTypes.func,
+};
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,33 +1,49 @@
 import { useSelector, useDispatch } from 'react-redux';
 import Book from '../components/Book';
-import { deleteBook } from '../actions';
+import CategoryFilter from '../components/CategoryFilter';
+import { deleteBook, changeFilter, removeFilter } from '../actions';
 
 export default function BooksList() {
   const dispatch = useDispatch();
+  const allBooks = useSelector((state) => state.bookReducer);
+  const filterBy = useSelector((state) => state.filterReducer);
+  const booksToRender = filterBy.filter === 'All' ? allBooks : allBooks.filter((book) => book.category === filterBy.filter);
 
   const handleRemoveBook = (book) => {
     dispatch(deleteBook(book));
   };
 
-  const allBooks = useSelector((state) => state.bookReducer);
+  const handleFilterChange = (catFilter) => {
+    if (catFilter === 'All') {
+      dispatch(removeFilter());
+    } else {
+      dispatch(changeFilter(catFilter));
+    }
+  };
+
   return (
-    <table>
-      <tbody>
-        <tr>
-          <th>Book ID</th>
-          <th>Title</th>
-          <th>Category</th>
-        </tr>
-        {allBooks.map((book) => (
-          <Book
-            id={book.id}
-            title={book.title}
-            category={book.category}
-            key={book.id}
-            clickHandler={handleRemoveBook}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <div>
+        <CategoryFilter category="Action" selectHandler={handleFilterChange} />
+      </div>
+      <table>
+        <tbody>
+          <tr>
+            <th>Book ID</th>
+            <th>Title</th>
+            <th>Category</th>
+          </tr>
+          {booksToRender.map((book) => (
+            <Book
+              id={book.id}
+              title={book.title}
+              category={book.category}
+              key={book.id}
+              clickHandler={handleRemoveBook}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
